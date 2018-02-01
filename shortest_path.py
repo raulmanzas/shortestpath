30eea4084975dd0eb6729d02c12e50cc55629a87
raul <raulmanzas@gmail.com>
2018-02-01 21:52:15 -0200

    Computation of a level's distances

--- shortest_path.py
+++ shortest_path.py
@@ -5,6 +5,10 @@ import argparse
 import yaml
 from mpi4py import MPI
 
+DISTANCES = 1
+WEIGHTS = 2
+NODE = 3
+
 def find_shortest_path(graph):
     """
     Computes the shortest path to each node in every level of the graph.
@@ -22,43 +26,31 @@ def find_shortest_path(graph):
     if rank == 0:
         levels, nodes, distances, weights = parse_graph_repr(graph)
         for level in range(2, levels + 1):
+            # initializes the distance array for this level
+            distances[level] = [0] * nodes
             for next_level_node in range(0, nodes):
                 destination = next_level_node + 1
-                comm.send(distances[level - 1], dest = destination)
-                comm.send(weights[level - 1], dest = destination)
-                comm.send(next_level_node, dest = destination)
+                comm.send(distances[level - 1], dest = destination, tag = DISTANCES)
+                comm.send(weights[level - 1], dest = destination, tag = WEIGHTS)
+                comm.send(next_level_node, dest = destination, tag = NODE)
+            
+            for next_level_node in range(0, nodes):
+                shortest_path = comm.recv(source = next_level_node + 1)
+                distances[level][next_level_node] = shortest_path
     else:
-        distances = comm.recv(source = 0)
-        weights = comm.recv(source = 0)
-        next_level_node = comm.recv(source = 0)
-        # data distribution between processes works fine
+        # Receives from master process all data needed to find the shortest path
+        # to node next_level_node
+        distances = comm.recv(source = 0, tag = DISTANCES)
+        weights = comm.recv(source = 0, tag = WEIGHTS)
+        next_level_node = comm.recv(source = 0, tag = NODE)
         local_distances = []
-        for node in range(0, len(distances)):
+
+        for node in range(0, num_processes - 1):
             dist = distances[node] + weights[node][next_level_node]
             local_distances.append(dist)
         
-        
-        print('I am process ' + str(rank) + ' and I received node ' + str(next_level_node))
-        print('Distances: ')
-        print(weights)
-
-
-    # for level in range(2, levels + 1):
-    #     distances[level] = []
-    #     # main process needs to send a copy of distances array and weights matrix to 'worker processes'
-    #     for next_level_node in range(0, nodes):
-    #         node_distances = [] # local in each process
-    #         for current_level_node in range(0, nodes):
-    #             # only the copy in this level should be sent to worker processes to save memory and comm time
-    #             distance = weights[level][current_level_node][next_level_node] + distances[level - 1][current_level_node]
-    #             node_distances.append(distance)
-    #         # after calculating all distances, find the smallest one and send to main process
-    #         shortest_path = min(node_distances)
-    #         distances[level].append(shortest_path)
-    
-    # print(distances)
-
-
+        # Computes the shorstest path to next_level_node and sends it back
+        comm.send(min(local_distances), 0)
 
 def parse_graph_repr(path):
     with open(path, 'r') as file:
@@ -78,4 +70,4 @@ if __name__ == '__main__':
     if args:
         find_shortest_path(args.graph)
     else:
-        print('You must pass an graph\' yaml representation')
+        print('You must pass an graph\'s yaml representation')
