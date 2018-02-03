fcf571a9e30de20d9814ff1f5e755c55d93ed3a6
raul <raulmanzas@gmail.com>
2018-02-02 22:54:42 -0200

    Graph representation generator

---
+++ tools/graph_generator.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import yaml
+import random
+
+RANDOM_MAX = 200
+
+def generate_graph_representation(levels, nodes, file_name):
+    # graph's naming convention
+    representation = "Levels: {0}\nNodesPerLevel: {1}\n".format(levels, nodes)
+    representation += "SourceDistances:\n"
+    for node in range(0, nodes):
+        value = random.randint(1, RANDOM_MAX)
+        representation += "  - {0}\n".format(value)
+    
+    representation += "Weights:\n"
+    for level in range(0, levels):
+        representation += "  {0}:\n    -\n".format(level + 1)
+        for node in range(0, nodes):
+            value = random.randint(1, RANDOM_MAX)
+            representation += "      - {0}\n".format(value)
+    
+    with open(file_name, 'w') as file:
+        file.write(representation)
+    print("Done!")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--levels', help='Number of levels')
+    parser.add_argument('--nodes', help='Number of nodes per level')
+    parser.add_argument('--name', help='Graph\'s representation name')
+    args = parser.parse_args()
+    if args:
+        levels = int(args.levels)
+        nodes = int(args.nodes)
+        file_name = args.name
+        generate_graph_representation(levels, nodes, file_name)
+    else:
+        print('You must pass an graph\'s yaml representation')
